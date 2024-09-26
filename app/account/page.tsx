aaa2254fb48d387dddc73f0a3ef913cc83aa270b
Leorev01 <revrennaleo@gmail.com>
2024-09-26 22:07:00 +0100

    Removes mistake code in Login component

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,13 +4,13 @@ import RegisterForm from "@/components/RegisterForm"
 
 const page = () => {
 
-  /*if(localStorage.user){
+  if(localStorage.user){
     return(
       <p>
         Account Page
       </p>
     )
-  }*/
+  }
   return (
     <section className='pt-[11rem] flex flex-col items-center'>
         <RegisterForm/>
