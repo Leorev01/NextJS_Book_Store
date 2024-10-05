4f21e95eed525856a15ce277ec78dc3d62c18165
Leorev01 <revrennaleo@gmail.com>
2024-10-05 15:23:15 +0100

    Adds some responsiveness to the stationary page

--- app/stationary/page.tsx
+++ app/stationary/page.tsx
@@ -5,12 +5,12 @@ import { stationary } from "@/data/stationary"
 
 const StationaryPage = () => {
   return (
-    <section className='pt-[11rem] flex flex-col items-center w-full'>
-        <div className='w-full h-0 border-x-[20rem] border-b-[3rem] border-transparent border-b-red-200 rotate-180 mb-0'></div>
+    <section className='pt-[11rem] flex flex-col items-center w-full mx-0 px-0'>
+        <div className='h-0 border-x-[20rem] border-b-[3rem] border-transparent border-b-red-200 rotate-180 m-0'></div>
         <h1 className="text-2xl font-bold relative bottom-10">
             Stationary
         </h1>
-        <div className="grid grid-cols-5 grid-flow-dense justify-evenly w-screen gap-20 px-10">
+        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 grid-flow-dense justify-evenly gap-20 px-10">
             {stationary.map((item) => (
                 <StationaryItem key={item.title} stationary={item} />
             ))}
