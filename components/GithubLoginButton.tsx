8ac6a6a3847e75ae401c9ace48889832b1a4d747
Leorev01 <revrennaleo@gmail.com>
2024-10-11 12:56:18 +0100

    Adjusts toasts for Google and Github Login

--- components/GithubLoginButton.tsx
+++ components/GithubLoginButton.tsx
@@ -6,10 +6,25 @@ import githubLogo from '@/public/images/github.svg';
 import { message } from 'antd';
 
 const GithubLoginButton = () => {
+  const handleLogin = async () => {
+    const result = await signIn('github', { redirect: false }); // Use redirect: false to prevent automatic navigation
+
+    if (result?.error) {
+      // Handle login error
+      message.error('Login failed. Please try again.');
+    } else {
+      // Successful login
+      message.success('Logged in successfully');
+    }
+  };
+
   return (
-    <Button onClick={() => {signIn('github'); message.success('Logged in successfully')}} className="w-[47%] text-blackborder gap-1 border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2">
-        <Image src={githubLogo} alt='github logo' width={20} height={20}/>
-        Github
+    <Button 
+      onClick={handleLogin} 
+      className="w-[47%] text-black border gap-1 border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2"
+    >
+      <Image src={githubLogo} alt='github logo' width={20} height={20} />
+      Github
     </Button>
   );
 };
--- components/GoogleLoginButton.tsx
+++ components/GoogleLoginButton.tsx
@@ -4,10 +4,25 @@ import Image from 'next/image';
 import googleLogo from '@/public/images/google.png';
 import { message } from 'antd';
 
-
 const GoogleLoginButton = () => {
+
+  const handleLogin = async () => {
+    const result = await signIn('google', { redirect: false }); // Prevent automatic redirection
+
+    if (result?.error) {
+      // Handle login error
+      message.error('Login failed. Please try again.');
+    } else {
+      // Successful login
+      message.success('Logged in successfully');
+    }
+  };
+
   return (
-    <Button onClick={() => {signIn('google'); message.success('Logged in successfully')}} className="w-[47%] text-black border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 flex items-center justify-center">
+    <Button 
+      onClick={handleLogin} // Use the new handleLogin function
+      className="w-[47%] text-black border-input bg-background hover:bg-accent hover:text-accent-foreground h-9 flex items-center justify-center"
+    >
       <Image src={googleLogo} alt="Google logo" width={20} height={20} />
       <span className="ml-2">Google</span>
     </Button>
