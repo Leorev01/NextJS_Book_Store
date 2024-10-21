138c76645b4db9ad737dee52ef02625e31344292
Leorev01 <revrennaleo@gmail.com>
2024-10-21 21:27:47 +0100

    Adds some styling to BookSection

--- components/PageBook.tsx
+++ components/PageBook.tsx
@@ -16,13 +16,15 @@ type bookProp = {
 
 const PageBook = ({ book }: { book: bookProp }) => {
     return (
-        <div className='mt-10 flex flex-col items-center min-w-[10rem] h-[18rem] md:h-[20rem] lg:h-[25rem]'> {/* Adjusted height here */}
+        <div className='mt-10 flex flex-col items-center min-w-[10rem] h-[18rem] md:h-[20rem] lg:h-[25rem] border-b-2 border-gray-300'> {/* Adjusted height here */}
             <div className='flex flex-col items-center hover:scale-105 duration-500 hover:cursor-pointer flex-grow p-0'>
                 <Link href={`/all/${book.id}`}><Image src={book.image} alt={book.title} width={100} height={100} className='md:w-[7rem] lg:w-[10rem]'/></Link>
                 <p className='text-center'>{book.title}</p>
                 <p className='font-bold'>£{book.price.toFixed(2)}</p>
             </div>
-            <AddButton item={book}/>
+            <div className='mb-5'>
+                <AddButton item={book}/>
+            </div>
         </div>
     )
 }
